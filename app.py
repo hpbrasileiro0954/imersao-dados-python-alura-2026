84d2d9f5b3aa30ea0c8520b077c933309f81f437
hpbrasileiro0954 <hpbrasileiro09@hotmail.com>
2026-01-31 22:42:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,15 +15,17 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
+# --- Configuração da Página ---
+# Define o título da página, o ícone e o layout para ocupar a largura inteira.
+st.set_page_config(
+    page_title="Dashboard de Salários na Área de Dados",
+    page_icon="📊",
+    layout="wide",
+)
+
 try:
+
     authenticator.login()
-    # --- Configuração da Página ---
-    # Define o título da página, o ícone e o layout para ocupar a largura inteira.
-    st.set_page_config(
-        page_title="Dashboard de Salários na Área de Dados",
-        page_icon="📊",
-        layout="wide",
-    )
     
     # --- Carregamento dos dados ---
     df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
